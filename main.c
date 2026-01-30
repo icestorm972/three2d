01183c21096fec395f8343d58e210353a5c50fea
di <git@diferrari.com>
2026-01-30 00:00:00 +0000

    [RAST] rasterizer

--- main.c
+++ main.c
@@ -8,9 +8,10 @@
 #include "types/vector4.h"
 #include "math/vector.h"
 #include "test.h"
+#include "math/aabb2.h"
 
 draw_ctx ctx = {};
-vector3 camera_pos = {0,0,2};
+vector3 camera_pos = {0,0,15};
 
 matrix4x4 proj_matrix;
 
@@ -35,6 +36,15 @@ vector4 vert_shader(vector3 pos, vector3 camera){
     return out_v;
 }
 
+argbcolor frag_shader(vector4 frag_coord, int trig_id){
+    return (argbcolor){
+        .alpha = 0xFF,
+        .red = (trig_id) % 255,
+        .green = (trig_id + 20) % 255,
+        .blue = (trig_id + 50) % 255,
+    };
+}
+
 static inline bool should_clip(vector4 v){//FIXME
     return  //v.x <= -v.w || v.x >= v.w ||
             // v.y <= -v.w || v.y >= v.w || 
@@ -43,11 +53,51 @@ static inline bool should_clip(vector4 v){//FIXME
             false;
 }
 
-void rasterize_triangle(vector3 v0, vector3 v1, vector3 v2){
-    //Stub
+static inline float triangle_area(vector2 a, vector2 b, vector2 c){
+    return 0.5f*((b.y-a.y)*(b.x+a.x) + (c.y-b.y)*(c.x+b.x) + (a.y-c.y)*(a.x+c.x));
 }
 
-void rasterize_quad(vector3 v0, vector3 v1, vector3 v2, vector3 v3){
+#define STEP 1
+
+void rasterize_triangle(vector2 v0, vector2 v1, vector2 v2, int trig_id){
+    aabb2 bb = {};
+    bb.min.x = floor(min(v0.x,min(v1.x,v2.x)));
+    bb.min.y = floor(min(v0.y,min(v1.y,v2.y)));
+    bb.max.x = ceil(max(v0.x,max(v1.x,v2.x)));
+    bb.max.y = ceil(max(v0.y,max(v1.y,v2.y)));
+    
+    bb.min.x = clampf(bb.min.x, 0, ctx.width-1);
+    bb.min.y = clampf(bb.min.y, 0, ctx.height-1);
+    bb.max.x = clampf(bb.max.x, 0, ctx.width-1);
+    bb.max.y = clampf(bb.max.y, 0, ctx.height-1);
+    
+    float total_area = triangle_area(v0, v1, v2);
+    if (total_area < 1) return;
+    
+    for (float y = bb.min.y; y <= bb.max.y; y += STEP){
+        for (float x = bb.min.x; x <= bb.max.x; x += STEP){
+            vector2 p = (vector2){x,y};
+            float a = triangle_area(v0, v1, p);
+            float b = triangle_area(v1, v2, p);
+            float c = triangle_area(v2, v0, p);
+            if (a < 0 || b < 0 || c < 0) continue;
+            ctx.fb[((int)y * ctx.width) + (int)x] = frag_shader((vector4){x,y,0,0},trig_id).color;
+            // fb_fill_rect(&ctx, x, y, STEP, STEP, );
+        }
+    }
+    
+    // fb_draw_line(&ctx, bb.min.x, bb.min.y, bb.max.x, bb.min.y, 0xFFFF0000);
+    // fb_draw_line(&ctx, bb.min.x, bb.min.y, bb.min.x, bb.max.y, 0xFFFF0000);
+    // fb_draw_line(&ctx, bb.min.x, bb.max.y, bb.max.x, bb.max.y, 0xFFFF0000);
+    // fb_draw_line(&ctx, bb.max.x, bb.min.y, bb.max.x, bb.max.y, 0xFFFF0000);
+    
+    // fb_draw_line(&ctx, v0.x, v0.y, v1.x, v1.y, 0xFFB4DD13);
+    // fb_draw_line(&ctx, v1.x, v1.y, v2.x, v2.y, 0xFFB4DD13);
+    // fb_draw_line(&ctx, v2.x, v2.y, v0.x, v0.y, 0xFFB4DD13);
+    
+}
+
+void rasterize_quad(vector2 v0, vector2 v1, vector2 v2, vector2 v3){
     //Stub
 }
 
@@ -77,28 +127,16 @@ tern draw(int segment_index, size_t num_segs, primitives prim_type, mesh *m, siz
     vector3 v2 = i2 < 0 ? (vector3){} : (vector3){c2.x/c2.w,c2.y/c2.w,c2.z/c2.w};
     vector3 v3 = i3 < 0 ? (vector3){} : (vector3){c3.x/c3.w,c3.y/c3.w,c3.z/c3.w};
     
-    float x0 = (v0.x+1)*0.5f*(screen.width-1);
-    float x1 = (v1.x+1)*0.5f*(screen.width-1);
-    
-    float y0 = (1-((v0.y+1)*0.5f))*(screen.height-1);
-    float y1 = (1-((v1.y+1)*0.5f))*(screen.height-1);
-    
-    fb_draw_line(&ctx, x0, y0, x1, y1, 0xFFB4DD13);
+    vector2 s0 = {(v0.x+1)*0.5f*(screen.width-1),(1-((v0.y+1)*0.5f))*(screen.height-1)};
+    vector2 s1 = {(v1.x+1)*0.5f*(screen.width-1),(1-((v1.y+1)*0.5f))*(screen.height-1)};
     
     if (i2 >= 0){
-        float x2 = (v2.x+1)*0.5f*(screen.width-1);
-        float y2 = (1-((v2.y+1)*0.5f))*(screen.height-1);
-        
-        fb_draw_line(&ctx, x1, y1, x2, y2, 0xFFB4DD13);
+        vector2 s2 = {(v2.x+1)*0.5f*(screen.width-1),(1-((v2.y+1)*0.5f))*(screen.height-1)};
         if (i3 >= 0){
-            float x3 = (v3.x+1)*0.5f*(screen.width-1);
-            float y3 = (1-((v3.y+1)*0.5f))*(screen.height-1);
-            fb_draw_line(&ctx, x2, y2, x3, y3, 0xFFB4DD13);
-            rasterize_quad(v0,v1,v2,v3);
-            fb_draw_line(&ctx, x3, y3, x0, y0, 0xFFB4DD13);
+            vector2 s3 = {(v3.x+1)*0.5f*(screen.width-1),(1-((v3.y+1)*0.5f))*(screen.height-1)};
+            rasterize_quad(s0,s1,s2,s3);
         } else {
-            fb_draw_line(&ctx, x2, y2, x0, y0, 0xFFB4DD13);
-            rasterize_triangle(v0,v1,v2);
+            rasterize_triangle(s0,s1,s2,segment_index);
         }
     }
     
@@ -144,6 +182,9 @@ int main(int argc, char* argv[]){
         for (size_t i = 0; i < num_segments; i++){
             if (draw((i * prim_type), num_segments * prim_type, prim_type, &m, num_verts, mid, (gpu_size){ctx.width,ctx.height}) == -1) return -1;
         }
+        
+        string_format_buf(buf,16,"%i FPS",(int)(1/dt));
+        fb_draw_string(&ctx, buf, 0,0, 2, 0xFFFFFFFF);
         commit_draw_ctx(&ctx);
 
         kbd_event ev = {};
@@ -154,14 +195,14 @@ int main(int argc, char* argv[]){
         } 
         mouse_data mouse = {};
         get_mouse_status(&mouse);
+        float adj_dt = minf(0.03f,dt);
         if (mouse_button_down(&mouse, 1)){
-            camera_pos.x += mouse.raw.x * dt;
-            camera_pos.y += mouse.raw.y * dt;
+            camera_pos.x += mouse.raw.x * adj_dt;
+            camera_pos.y += mouse.raw.y * adj_dt;
         }
-        camera_pos.z -= mouse.raw.scroll * 2500 * dt;
+        camera_pos.z -= mouse.raw.scroll * 2500 * adj_dt;
         memset(buf, 0, 16);
-        string_format_buf(buf,16,"%i FPS",(int)(1/dt));
-        fb_draw_string(&ctx, buf, 0,0, 2, 0xFFFFFFFF);
+        
     }
     destroy_draw_ctx(&ctx);
     return 0;
